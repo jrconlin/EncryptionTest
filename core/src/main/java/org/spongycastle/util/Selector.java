6ae5c7f665fc4ddf09f95e3001efc49e1514d6e2
jrconlin <jrconlin+git@mozilla.com>
2015-10-02 11:29:44 -0700

    remove unused sources

--- core/src/main/java/org/spongycastle/util/Selector.java
+++
@@ -1,9 +0,0 @@
-package org.spongycastle.util;
-
-public interface Selector<T>
-    extends Cloneable
-{
-    boolean match(T obj);
-
-    Object clone();
-}
--- pg/src/main/java/org/spongycastle/openpgp/operator/PBEProtectionRemoverFactory.java
+++
@@ -1,9 +0,0 @@
-package org.spongycastle.openpgp.operator;
-
-import org.spongycastle.openpgp.PGPException;
-
-public interface PBEProtectionRemoverFactory
-{
-    PBESecretKeyDecryptor createDecryptor(String protection)
-        throws PGPException;
-}
